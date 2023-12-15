6b43d8455e9c1450eb5239b9c1d8bec7700348b4
eltoncampos1 <eltoncampos36@gmail.com>
2023-12-15 18:46:16 -0300

    get links by hash

--- lib/ex_short/links.ex
+++ lib/ex_short/links.ex
@@ -1,10 +1,18 @@
 defmodule ExShort.Links do
   alias ExShort.Repo
   alias ExShort.Links.Link
+  import Ecto.Query
 
   def create(params) do
     %Link{}
     |> Link.changeset(params)
     |> Repo.insert()
+  end
+
+  def get_url_by_hash(hash) do
+    from(l in Link, as: :link)
+    |> where([link: l], l.hash == ^hash)
+    |> limit(1)
+    |> Repo.one()
   end
 end
